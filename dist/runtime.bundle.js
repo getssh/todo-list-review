1323b219749f5891f78d4f0f0ba6801f8bd61ef5
getssh <gtamrat33@gmail.com>
2023-02-27 23:22:29 +0300

    Deploy todo-list-webapp on github

--- dist/runtime.bundle.js
+++ dist/runtime.bundle.js
@@ -158,7 +158,7 @@
 /******/ 			return __webpack_require__.O(result);
 /******/ 		}
 /******/ 		
-/******/ 		var chunkLoadingGlobal = self["webpackChunktodo_list_structure"] = self["webpackChunktodo_list_structure"] || [];
+/******/ 		var chunkLoadingGlobal = self["webpackChunktodo_list_review"] = self["webpackChunktodo_list_review"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
 /******/ 	})();
